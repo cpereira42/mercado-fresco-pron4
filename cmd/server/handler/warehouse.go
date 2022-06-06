3f4466b32f31e19fd12fcf0991098454a5b1213c
Vinicius Oliveira <runnice@gmail.com>
2022-06-06 15:22:29 -0300

    changed error responses using web pkg

--- cmd/server/handler/warehouse.go
+++ cmd/server/handler/warehouse.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 
 	"github.com/cpereira42/mercado-fresco-pron4/internal/warehouse"
+	"github.com/cpereira42/mercado-fresco-pron4/pkg/web"
 	"github.com/gin-gonic/gin"
 )
 
@@ -30,10 +31,10 @@ func NewWarehouse(w warehouse.Service) *Warehouse {
 func (c *Warehouse) GetAll(ctx *gin.Context) {
 	w, err := c.service.GetAll()
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		ctx.JSON(http.StatusInternalServerError, web.NewResponse(http.StatusNotFound, nil, err.Error()))
 		return
 	}
-	ctx.JSON(http.StatusOK, w)
+	ctx.JSON(http.StatusOK, web.NewResponse(http.StatusOK, w, ""))
 
 }
 
@@ -41,31 +42,32 @@ func (c *Warehouse) Create(ctx *gin.Context) {
 	var r request
 	if err := ctx.ShouldBindJSON(&r); err != nil {
 		if err == io.EOF {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "the body cannot be empty"})
+			ctx.JSON(http.StatusBadRequest, web.NewResponse(
+				http.StatusBadRequest, nil, "Error reading request body"),
+			)
 			return
 		}
-		ctx.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"error": err.Error()})
+		ctx.AbortWithStatusJSON(http.StatusUnprocessableEntity, web.NewResponse(http.StatusUnprocessableEntity, nil, err.Error()))
 		return
 	}
 	w, err := c.service.Create(r.ID, r.Address, r.Telephone, r.Warehouse_code, r.Minimum_capacity, r.Minimum_temperature)
 	if err != nil {
-		ctx.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+		ctx.AbortWithStatusJSON(http.StatusUnprocessableEntity, web.NewResponse(http.StatusUnprocessableEntity, nil, err.Error()))
 		return
 	}
-	ctx.JSON(http.StatusCreated, w)
+	ctx.JSON(http.StatusCreated, web.NewResponse(http.StatusCreated, w, ""))
 }
 
 func (c *Warehouse) Update(ctx *gin.Context) {
 	id, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
-		ctx.JSON(http.StatusNotFound, gin.H{"error": "Warehouse Not Found"})
+		ctx.JSON(http.StatusNotFound, web.NewResponse(http.StatusNotFound, nil, "Warehouse Not Found"))
 		return
 	}
 
 	var r warehouse.Warehouse
 	if err := ctx.ShouldBindJSON(&r); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, "Error reading request body"))
 	}
 
 	// if r.Address == "" {
@@ -86,38 +88,38 @@ func (c *Warehouse) Update(ctx *gin.Context) {
 
 	w, err := c.service.Update(id, r.Address, r.Telephone, r.Warehouse_code, r.Minimum_capacity, r.Minimum_temperature)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, "Warehouse Code already exists"))
 		return
 	}
-	ctx.JSON(http.StatusOK, w)
+	ctx.JSON(http.StatusOK, web.NewResponse(http.StatusOK, w, ""))
 }
 
 func (c *Warehouse) GetByID(ctx *gin.Context) {
 	id, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
+		ctx.JSON(http.StatusNotFound, web.NewResponse(http.StatusNotFound, nil, "Invalid ID"))
 		return
 	}
 
 	w, err := c.service.GetByID(id)
 
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
 		return
 	}
-	ctx.JSON(http.StatusOK, w)
+	ctx.JSON(http.StatusOK, web.NewResponse(http.StatusOK, w, ""))
 }
 
 func (c *Warehouse) Delete(ctx *gin.Context) {
 	id, err := strconv.Atoi(ctx.Param("id"))
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid id"})
+		ctx.JSON(http.StatusNotFound, web.NewResponse(http.StatusNotFound, nil, "Invalid ID"))
 		return
 	}
 	err = c.service.Delete(id)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
 		return
 	}
-	ctx.JSON(http.StatusNoContent, gin.H{})
+	ctx.JSON(http.StatusNoContent, web.NewResponse(http.StatusNoContent, nil, ""))
 }
