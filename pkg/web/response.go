402c14cfc669dc9f77774871930b60220b5b711e
Eduardo de Araújo <eduardo.araujo@mercadolivre.com>
2022-06-08 13:15:26 -0300

    fixed erros in response and transfer the logic from repository to the controller

--- pkg/web/response.go
+++ pkg/web/response.go
@@ -1,18 +1,52 @@
 package web
 
 import (
-	"strconv"
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 type Response struct {
-	Code  string      `json:"code"`
+	Code  int         `json:"code"`
 	Data  interface{} `json:"data,omitempty"`
-	Error string      `json:"error,omitempty"`
+	Error any         `json:"error,omitempty"`
 }
 
-func NewResponse(code int, data interface{}, err string) Response {
+type ApiError struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+func NewResponse(code int, data interface{}, err any) Response {
 	if code < 300 {
-		return (Response{strconv.FormatInt(int64(code), 10), data, ""})
+		return (Response{code, data, ""})
 	}
-	return (Response{strconv.FormatInt(int64(code), 10), nil, err})
+	return (Response{code, nil, err})
+}
+
+func msgForTag(tag string) string {
+	switch tag {
+	case "required":
+		return "This field is required"
+	case "numeric":
+		return "This field only accepts numbers"
+	}
+	return ""
+}
+
+func CheckIfErrorRequest(ctx *gin.Context, req any) bool {
+	if err := ctx.ShouldBind(&req); err != nil {
+		var ve validator.ValidationErrors
+		if errors.As(err, &ve) {
+			out := make([]ApiError, len(ve))
+			for i, fe := range ve {
+				out[i] = ApiError{fe.Field(), msgForTag(fe.Tag())}
+			}
+			ctx.JSON(http.StatusUnprocessableEntity, NewResponse(http.StatusUnprocessableEntity, nil, out))
+		}
+		return true
+	}
+	return false
 }
