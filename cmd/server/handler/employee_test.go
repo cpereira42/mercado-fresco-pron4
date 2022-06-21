f1141c7706f411040553dfc6b52dc6b7b9e2f8ef
Eduardo de Araújo <eduardo.araujo@mercadolivre.com>
2022-06-21 12:25:02 -0300

    GetAll Handler Tests

---
+++ cmd/server/handler/employee_test.go
@@ -0,0 +1,85 @@
+package handler_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/cpereira42/mercado-fresco-pron4/cmd/server/handler"
+	"github.com/cpereira42/mercado-fresco-pron4/internal/employee"
+	"github.com/cpereira42/mercado-fresco-pron4/internal/employee/mocks"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	employee1 = employee.Employee{ID: 1, CardNumberID: "123", FirstName: "Eduardo", LastName: "Araujo", WarehouseID: 1}
+
+	employee2 = employee.Employee{ID: 2, CardNumberID: "1234", FirstName: "Maria", LastName: "Silva", WarehouseID: 2}
+
+	employee3 = employee.Employee{ID: 3, CardNumberID: "12345", FirstName: "Jefferson", LastName: "Filho", WarehouseID: 1}
+
+	newEmployee = employee.Employee{CardNumberID: "123456", FirstName: "Marta", LastName: "Gomes", WarehouseID: 3}
+
+	employee1Update = employee.Employee{ID: 1, CardNumberID: "123", FirstName: "Gustavo", LastName: "Junior", WarehouseID: 1}
+
+	employeeUpdateSameCardNumberID = employee.Employee{ID: 2, CardNumberID: "123", FirstName: "Gustavo", LastName: "Junior", WarehouseID: 1}
+)
+
+func createRequestTest(method string, url string, body string) (*http.Request, *httptest.ResponseRecorder) {
+	req := httptest.NewRequest(method, url, bytes.NewBuffer([]byte(body)))
+	req.Header.Add("Content-Type", "application/json")
+	return req, httptest.NewRecorder()
+}
+
+func TestHandlerGetAll(t *testing.T) {
+	employees := []employee.Employee{employee1, employee2, employee3}
+
+	t.Run("If request GetAll is OK, it should return status code 200 and a list of employees", func(t *testing.T) {
+		req, rr := createRequestTest(http.MethodGet, "/api/v1/employees/", "")
+		serviceMock := &mocks.Service{}
+		serviceMock.On("GetAll").Return(employees, nil)
+
+		e := handler.NewEmployee(serviceMock)
+		r := gin.Default()
+		employeeGroup := r.Group("/api/v1/employees/")
+		employeeGroup.GET("/", e.GetAll())
+
+		r.ServeHTTP(rr, req)
+		assert.Equal(t, 200, rr.Code)
+
+		jsonResponse := struct {
+			Code int
+			Data []employee.Employee
+		}{}
+		err := json.Unmarshal(rr.Body.Bytes(), &jsonResponse)
+		assert.Equal(t, jsonResponse.Data, employees)
+		assert.Nil(t, err)
+		assert.True(t, len(jsonResponse.Data) > 0)
+	})
+	t.Run("If request GetAll has an error, it should return status code 404 and an error", func(t *testing.T) {
+		errorMsg := fmt.Errorf("error to get all employees")
+		req, rr := createRequestTest(http.MethodGet, "/api/v1/employees/", "")
+		serviceMock := &mocks.Service{}
+		serviceMock.On("GetAll").Return([]employee.Employee{}, errorMsg)
+
+		e := handler.NewEmployee(serviceMock)
+		r := gin.Default()
+		employeeGroup := r.Group("/api/v1/employees/")
+		employeeGroup.GET("/", e.GetAll())
+
+		r.ServeHTTP(rr, req)
+		assert.Equal(t, 404, rr.Code)
+
+		jsonResponse := struct {
+			Code  int
+			Error string
+		}{}
+		err := json.Unmarshal(rr.Body.Bytes(), &jsonResponse)
+		assert.Nil(t, err)
+		assert.Equal(t, jsonResponse.Error, errorMsg.Error())
+	})
+}
