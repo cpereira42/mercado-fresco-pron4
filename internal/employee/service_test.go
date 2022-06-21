634ddda7cdf62d23a307e48bfa0361b03079a2b8
Eduardo de Araújo <eduardo.araujo@mercadolivre.com>
2022-06-21 09:28:37 -0300

    GetByID Service Tests

--- internal/employee/service_test.go
+++ internal/employee/service_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/cpereira42/mercado-fresco-pron4/internal/employee"
 	"github.com/cpereira42/mercado-fresco-pron4/internal/employee/mocks"
 	"github.com/stretchr/testify/assert"
+	tmock "github.com/stretchr/testify/mock"
 )
 
 var (
@@ -23,9 +24,9 @@ var (
 	employeeUpdateSameCardNumberID = employee.Employee{ID: 1, CardNumberID: "1235", FirstName: "Gustavo", LastName: "Junior", WarehouseID: 1}
 )
 
-func Test_RepositoryGetAll(t *testing.T) {
+func TestServiceGetAll(t *testing.T) {
 	employees := []employee.Employee{employee1, employee2, employee3}
-	t.Run("Get all employees Ok", func(t *testing.T) {
+	t.Run("If GetAll is ok, it should return a list of employees", func(t *testing.T) {
 		repo := &mocks.Repository{}
 		repo.On("GetAll").Return(employees, nil)
 
@@ -38,16 +39,75 @@ func Test_RepositoryGetAll(t *testing.T) {
 
 	})
 
-	t.Run("Get all employees error", func(t *testing.T) {
-		errorMsg := fmt.Errorf("Cannot get all")
+	t.Run("If GetAll has an error, it should return an error", func(t *testing.T) {
+		errorMsg := fmt.Errorf("Failed to get all employees")
 		repo := &mocks.Repository{}
 		repo.On("GetAll").Return([]employee.Employee{}, errorMsg).Once()
 
 		service := employee.NewService(repo)
-		_, err := service.GetAll()
+		employeesService, err := service.GetAll()
 
-		assert.Error(t, err)
-		assert.Equal(t, err, errorMsg)
+		assert.True(t, len(employeesService) == 0)
+		assert.Equal(t, fmt.Errorf("Failed to get all employees"), err)
+		repo.AssertExpectations(t)
 
 	})
+}
+
+func TestServiceGetByID(t *testing.T) {
+	employees := []employee.Employee{employee1, employee2, employee3}
+	t.Run("If GetByID is success, it should return a employee",
+		func(t *testing.T) {
+			repo := &mocks.Repository{}
+			repo.On("GetAll").Return(employees, nil).Once()
+			repo.On("GetByID", tmock.AnythingOfType("int")).Return(employee1, nil).Once()
+
+			service := employee.NewService(repo)
+			employeeByID, err := service.GetByID(1)
+
+			assert.NoError(t, err)
+			assert.Equal(t, employee1, employeeByID)
+
+		})
+	t.Run("If GetByID has an error to get all employees, it should return an error",
+		func(t *testing.T) {
+			errorMsg := fmt.Errorf("Failed to get all employees in the GetByID ")
+			repo := &mocks.Repository{}
+			repo.On("GetAll").Return([]employee.Employee{}, errorMsg).Once()
+
+			service := employee.NewService(repo)
+			_, err := service.GetByID(1)
+
+			assert.Error(t, err)
+			assert.EqualError(t, err, errorMsg.Error())
+
+		})
+	t.Run("If the id in the GetByID does not exists, it should return an error",
+		func(t *testing.T) {
+			errorMsg := fmt.Errorf("user with id 10 not found")
+			repo := &mocks.Repository{}
+			repo.On("GetAll").Return(employees, nil).Once()
+			repo.On("GetByID", tmock.AnythingOfType("int")).Return(employee.Employee{}, errorMsg).Once()
+
+			service := employee.NewService(repo)
+			_, err := service.GetByID(10)
+
+			assert.Error(t, err)
+			assert.EqualError(t, err, errorMsg.Error())
+
+		})
+
+	t.Run("If GetByID has an error, it should return an error",
+		func(t *testing.T) {
+			errorMsg := fmt.Errorf("error to GetByID")
+			repo := &mocks.Repository{}
+			repo.On("GetAll").Return(employees, nil).Once()
+			repo.On("GetByID", tmock.AnythingOfType("int")).Return(employee.Employee{}, errorMsg).Once()
+
+			service := employee.NewService(repo)
+			_, err := service.GetByID(1)
+
+			assert.Error(t, err)
+			assert.EqualError(t, err, errorMsg.Error())
+		})
 }
