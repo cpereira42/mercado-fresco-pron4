9f8f80e740437a966b0b871082031059768948c8
Vinicius Oliveira <runnice@gmail.com>
2022-06-27 20:06:27 -0300

    modified update on service and remaked all tests on controller and service

--- internal/warehouse/repository.go
+++ internal/warehouse/repository.go
@@ -3,6 +3,7 @@ package warehouse
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -10,20 +11,21 @@ import (
 
 type Repository interface {
 	GetAll() ([]Warehouse, error)
-	Create(id int, address, telephone, warehouse_code string, minimum_capacity, minimum_temperature int) (Warehouse, error)
-	LastID() (int, error)
-	Update(id int, address, telephone, warehouse_code string, minimum_capacity, minimum_temperature int) (Warehouse, error)
+	Create(id int, address, telephone, warehouse_code string, minimum_capacity, minimum_temperature, locality_id int) (Warehouse, error)
+	//LastID() (int, error)
+	Update(id int, address, telephone, warehouse_code string, minimum_capacity, minimum_temperature, locality_id int) (Warehouse, error)
 	GetByID(id int) (Warehouse, error)
 	Delete(id int) error
+	CheckLocality(id int) (bool, error)
 }
 
 const (
-	GetAll          = "SELECT id, address, telephone, warehouse_code, minimum_capacity, minimum_temperature FROM warehouse"
-	LastID          = "SELECT MAX(id) FROM warehouse"
-	CreateWarehouse = "INSERT INTO warehouse (address, telephone, warehouse_code, minimum_capacity, minimum_temperature) VALUES (?, ?, ?, ?, ?)"
-	UpdateWarehouse = "UPDATE warehouse SET address=?, telephone=?, warehouse_code=?, minimum_capacity=?, minimum_temperature=? WHERE id=?"
-	GetId           = "SELECT id, address, telephone, warehouse_code, minimum_capacity, minimum_temperature FROM warehouse WHERE id=?"
-	DeleteWarehouse = "DELETE FROM warehouse WHERE id=?"
+	GetAll = `SELECT id, address, telephone, warehouse_code, minimum_capacity, minimum_temperature, locality_id FROM warehouse`
+	//LastID          = "SELECT MAX(id) FROM warehouse"
+	CreateWarehouse = `INSERT INTO warehouse (address, telephone, warehouse_code, minimum_capacity, minimum_temperature, locality_id) VALUES (?, ?, ?, ?, ?, ?)`
+	UpdateWarehouse = `UPDATE warehouse SET address=?, telephone=?, warehouse_code=?, minimum_capacity=?, minimum_temperature=?, locality_id=? WHERE id=?`
+	GetId           = `SELECT id, address, telephone, warehouse_code, minimum_capacity, minimum_temperature FROM warehouse WHERE id=?`
+	DeleteWarehouse = `DELETE FROM warehouse WHERE id=?`
 )
 
 //var wr []Warehouse
@@ -50,7 +52,7 @@ func (r *repository) GetAll() ([]Warehouse, error) {
 
 	for rows.Next() {
 
-		err := rows.Scan(&w.ID, &w.Address, &w.Telephone, &w.Warehouse_code, &w.Minimum_capacity, &w.Minimum_temperature)
+		err := rows.Scan(&w.ID, &w.Address, &w.Telephone, &w.Warehouse_code, &w.Minimum_capacity, &w.Minimum_temperature, &w.Locality_id)
 		if err != nil {
 			return []Warehouse{}, err
 		}
@@ -60,34 +62,33 @@ func (r *repository) GetAll() ([]Warehouse, error) {
 	return wr, nil
 }
 
-func (r *repository) LastID() (int, error) {
-	var maxCount int
+// func (r *repository) LastID() (int, error) {
+// 	var maxCount int
 
-	row := r.db.QueryRow(LastID)
-	err := row.Scan(&maxCount)
-	if err != nil {
-		return 0, err
-	}
-	return maxCount, nil
-}
+// 	row := r.db.QueryRow(LastID)
+// 	err := row.Scan(&maxCount)
+// 	if err != nil {
+// 		return 0, err
+// 	}
+// 	return maxCount, nil
+// }
 
-func (r *repository) Create(id int, address, telephone, warehouse_code string, minimum_capacity, minimum_temperature int) (Warehouse, error) {
+func (r *repository) Create(id int, address, telephone, warehouse_code string, minimum_capacity, minimum_temperature, locality_id int) (Warehouse, error) {
 
-	w = Warehouse{id, address, telephone, warehouse_code, minimum_capacity, minimum_temperature}
+	w = Warehouse{id, address, telephone, warehouse_code, minimum_capacity, minimum_temperature, locality_id}
 
-	stmt, err := r.db.Prepare(CreateWarehouse)
+	stmt, err := r.db.Exec(CreateWarehouse, &w.Address, &w.Telephone, &w.Warehouse_code, &w.Minimum_capacity, &w.Minimum_temperature, locality_id)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer stmt.Close()
-
-	var result sql.Result
-
-	result, err = stmt.Exec(&w.Address, &w.Telephone, &w.Warehouse_code, &w.Minimum_capacity, &w.Minimum_temperature)
+	rowsAffected, err := stmt.RowsAffected()
 	if err != nil {
 		return Warehouse{}, err
 	}
-	insertedId, err := result.LastInsertId()
+	if rowsAffected == 0 {
+		return Warehouse{}, errors.New("no rows affected")
+	}
+	insertedId, err := stmt.LastInsertId()
 	if err != nil {
 		return Warehouse{}, err
 	}
@@ -96,26 +97,19 @@ func (r *repository) Create(id int, address, telephone, warehouse_code string, m
 
 }
 
-func (r *repository) Update(id int, address, telephone, warehouse_code string, minimum_capacity, minimum_temperature int) (Warehouse, error) {
-	w := Warehouse{id, address, telephone, warehouse_code, minimum_capacity, minimum_temperature}
+func (r *repository) Update(id int, address, telephone, warehouse_code string, minimum_capacity, minimum_temperature, locality_id int) (Warehouse, error) {
+	w := Warehouse{id, address, telephone, warehouse_code, minimum_capacity, minimum_temperature, locality_id}
 
-	stmt, err := r.db.Prepare(UpdateWarehouse)
+	stmt, err := r.db.Exec(UpdateWarehouse, &w.Address, &w.Telephone, &w.Warehouse_code, &w.Minimum_capacity, &w.Minimum_temperature, &w.Locality_id, id)
 	if err != nil {
 		return Warehouse{}, err
 	}
-	defer stmt.Close()
-
-	rows, err := stmt.Exec(&w.Address, &w.Telephone, &w.Warehouse_code, &w.Minimum_capacity, &w.Minimum_temperature, &w.ID)
+	rowsAffected, err := stmt.RowsAffected()
 	if err != nil {
 		return Warehouse{}, err
 	}
-
-	lines, err := rows.RowsAffected()
-	if err != nil {
-		return Warehouse{}, err
-	}
-	if lines == 0 {
-		return Warehouse{}, sql.ErrNoRows
+	if rowsAffected == 0 {
+		return Warehouse{}, errors.New("no rows affected")
 	}
 
 	return w, nil
@@ -165,4 +159,27 @@ func (r *repository) Delete(id int) error {
 	}
 
 	return nil
+}
+
+func (r *repository) CheckLocality(id int) (bool, error) {
+
+	type Locality struct {
+		Id           int
+		LocalityName string
+		ProvinceId   int
+	}
+
+	stmt, err := r.db.Prepare(`SELECT id, locality_name, province_id FROM localities WHERE id = ?`)
+	if err != nil {
+		return false, err
+	}
+	defer stmt.Close()
+
+	var locality Locality
+
+	err = stmt.QueryRow(id).Scan(&locality.Id, &locality.LocalityName, &locality.ProvinceId)
+	if err != nil {
+		return false, fmt.Errorf("locality %d not found", id)
+	}
+	return true, nil
 }
