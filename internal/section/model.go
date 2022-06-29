05c677f560bbc57116ca742b5dedcf46186bcc30
eneassena10 <eneas.sena@mercadolivre.com>
2022-06-29 14:37:20 -0300

    productBatches implementation

--- internal/section/model.go
+++ internal/section/model.go
@@ -5,95 +5,44 @@ package section
  * Entidade: Section{}, [usado para respostas do db no repository]
  * Request POST: SectionRequestCreate{}
  * Request PATCH: SectionRequestUpdate{}
+ * Request POST: ProductBatches{}
+ * Request GET: ProductBatchesResponse{}
  */
 type Section struct {
-	Id                 int     `json:"id" binding:"numeric,omitempty"`
-	SectionNumber      string  `json:"section_number" binding:"alfa,omitempty"`
-	CurrentTemperature float64 `json:"current_temperature" binding:"numeric,omitempty"`
-	MinimumTemperature float64 `json:"minimum_temperature" binding:"numeric,omitempty"`
-	CurrentCapacity    int     `json:"current_capacity" binding:"numeric,omitempty"`
-	MinimumCapacity    int     `json:"minimum_capacity" binding:"numeric,omitempty"`
-	MaximumCapacity    int     `json:"maximum_capacity" binding:"numeric,omitempty"`
-	WarehouseId        int     `json:"warehouse_id" binding:"numeric,omitempty"`
-	ProductTypeId      int     `json:"product_type_id" binding:"numeric,omitempty"`
+	Id                 int64 `json:"id,omitempty" binding:"numeric,omitempty"`
+	SectionNumber      int   `json:"section_number" binding:"alfa,omitempty"`
+	CurrentTemperature int   `json:"current_temperature" binding:"numeric,omitempty"`
+	MinimumTemperature int   `json:"minimum_temperature" binding:"numeric,omitempty"`
+	CurrentCapacity    int   `json:"current_capacity" binding:"numeric,omitempty"`
+	MinimumCapacity    int   `json:"minimum_capacity" binding:"numeric,omitempty"`
+	MaximumCapacity    int   `json:"maximum_capacity" binding:"numeric,omitempty"`
+	WarehouseId        int64 `json:"warehouse_id" binding:"numeric,omitempty"`
+	ProductTypeId      int64 `json:"product_type_id" binding:"numeric,omitempty"`
 }
 type SectionRequestCreate struct {
-	SectionNumber      string  `json:"section_number" binding:"required,alphanum"`
-	CurrentTemperature float64 `json:"current_temperature" binding:"required,numeric"`
-	MinimumTemperature float64 `json:"minimum_temperature" binding:"required,numeric"`
-	CurrentCapacity    int     `json:"current_capacity" binding:"required,numeric"`
-	MinimumCapacity    int     `json:"minimum_capacity" binding:"required,numeric"`
-	MaximumCapacity    int     `json:"maximum_capacity" binding:"required,numeric"`
-	WarehouseId        int     `json:"warehouse_id" binding:"required,numeric"`
-	ProductTypeId      int     `json:"product_type_id" binding:"required,numeric"`
+	SectionNumber      int   `json:"section_number" binding:"required,numeric"`
+	CurrentTemperature int   `json:"current_temperature" binding:"required,numeric"`
+	MinimumTemperature int   `json:"minimum_temperature" binding:"required,numeric"`
+	CurrentCapacity    int   `json:"current_capacity" binding:"required,numeric"`
+	MinimumCapacity    int   `json:"minimum_capacity" binding:"required,numeric"`
+	MaximumCapacity    int   `json:"maximum_capacity" binding:"required,numeric"`
+	WarehouseId        int64 `json:"warehouse_id" binding:"required,numeric"`
+	ProductTypeId      int64 `json:"product_type_id" binding:"required,numeric"`
 }
 type SectionRequestUpdate struct {
-	SectionNumber      string  `json:"section_number" binding:"required,numeric,omitempty"`
-	CurrentTemperature float64 `json:"current_temperature" binding:"numeric,omitempty"`
-	MinimumTemperature float64 `json:"minimum_temperature" binding:"numeric,omitempty"`
-	CurrentCapacity    int     `json:"current_capacity" binding:"numeric,omitempty"`
-	MinimumCapacity    int     `json:"minimum_capacity" binding:"numeric,omitempty"`
-	MaximumCapacity    int     `json:"maximum_capacity" binding:"numeric,omitempty"`
-	WarehouseId        int     `json:"warehouse_id" binding:"numeric,omitempty"`
-	ProductTypeId      int     `json:"product_type_id" binding:"numeric,omitempty"`
+	SectionNumber      int   `json:"section_number" binding:"required,numeric,omitempty"`
+	CurrentTemperature int   `json:"current_temperature" binding:"numeric,omitempty"`
+	MinimumTemperature int   `json:"minimum_temperature" binding:"numeric,omitempty"`
+	CurrentCapacity    int   `json:"current_capacity" binding:"numeric,omitempty"`
+	MinimumCapacity    int   `json:"minimum_capacity" binding:"numeric,omitempty"`
+	MaximumCapacity    int   `json:"maximum_capacity" binding:"numeric,omitempty"`
+	WarehouseId        int64 `json:"warehouse_id" binding:"numeric,omitempty"`
+	ProductTypeId      int64 `json:"product_type_id" binding:"numeric,omitempty"`
 }
-
-/*
-	novos requisitos
-
-	É necessário que os Product_Batches sejam compostos por • id: Identificación unica
-	• batch_number: número do lote
-	• current_quantity: quantidade atual por lote
-	• current_temperature: Temperatura atual.
-	• due_date: data de validade do produto
-	• initial_quantity: quantidade inicial
-	• manufacturing_date: data de fabricação
-	• manufacturing_hour: hora de fabricação
-	• minumum_temperature: Temperatura mínima • product_id: ID do produto
-	• section_id: ID da seção
-
-	restrições de criação
-	* o product id deve existir
-	* o section id deve existir
-	batch_number é um campo único
-
-	etapa de criação
-	POST |	/api/v1/productBatches
-	* Quando a entrada de dados for bem-sucedida, um código 201 será retornado junto com o objeto inserido.
-	* Se o batch_number já existir, ele retornará um erro 409 Conflict.
-	* Se o objeto JSON não contiver os campos necessários, um código 422 será retornado.
-
-	GET  |	/api/v1/sections/reportProduc ts?id=1
-	* Quando a solicitação for bem-sucedida, o backend retornará um relatório com o número de Produtos em
-		cada Seção ou o número de produtos de uma determinada seção.
-
-
-	Request :
-	{
-		"batch_number": 111,
-		"current_quantity": 200,
-		"current_temperature": 20,
-		"due_date": "2022-04-04",
-		"initial_quantity": 10,
-		"manufacturing_date": "2020-04-04",
-		"manufacturing_hour": 10,
-		"minumum_temperature":5,
-		"product_id": 1,
-		"section_id": 1
-	}
-
-	Response:
-	{
-		"section_id": 1,
-		"section_number": 23,
-		"products_count": 200
-	}
-
-*/
-
 type ProductBatches struct {
 	BatchNumber        int    `json:"batch_number" binding:"required,numeric"`
 	CurrentQuantity    int    `json:"current_quantity" binding:"required,numeric"`
+	CurrentTemperature int    `json:"current_temperature" binding:"required"`
 	DueDate            string `json:"due_date" binding:"require,alpha"`
 	InitialQuantity    int    `json:"initial_quantity" binding:"required,numeric"`
 	ManufacturingDate  string `json:"manufacturing_date" binding:"required,numeric"`
@@ -103,7 +52,7 @@ type ProductBatches struct {
 	SectionId          int    `json:"section_id" binding:"required,numeric"`
 }
 
-type ProdctuBatchesResponse struct {
+type ProductBatchesResponse struct {
 	SectionId     int `json:"section_id" binding:"required"`
 	SectionNumber int `json:"section_number" binding:"required"`
 	ProductsCount int `json:"products_count" binding:"required"`
@@ -112,32 +61,41 @@ type ProdctuBatchesResponse struct {
 /*
  * Estrutura do repository.go
  * Repository interface{}
- * repository struct{}
  */
-
 type Repository interface {
 	ListarSectionAll() ([]Section, error)
-	ListarSectionOne(id int) (Section, error)
 	CreateSection(section Section) (Section, error)
-	UpdateSection(id int, section Section) (Section, error)
-	DeleteSection(id int) error
+	UpdateSection(section Section) (Section, error)
+	ListarSectionOne(id int64) (Section, error)
+	DeleteSection(id int64) error
+}
+type RepositoryProductBatches interface {
+	CreatePB(productBatches ProductBatches) (ProductBatches, error)
+	ReadPB(sectionId int64) (ProductBatchesResponse, error)
+}
+
+type ProductTypes struct {
+	ID          int    `json:"id"`
+	Description string `json:"description"`
 }
 
 /*
  * estrutura do service.go
  *	Service interface{}
- * service struct{}
  */
-
 type Service interface {
 	ListarSectionAll() ([]Section, error)
-	ListarSectionOne(id int) (Section, error)
 	CreateSection(section SectionRequestCreate) (Section, error)
-	UpdateSection(id int, sectionUp SectionRequestUpdate) (Section, error)
-	DeleteSection(id int) error
+	ListarSectionOne(id int64) (Section, error)
+	UpdateSection(id int64, sectionUp SectionRequestUpdate) (Section, error)
+	DeleteSection(id int64) error
+}
+type ServicePB interface {
+	CreatePB(productBatches ProductBatches) (ProductBatches, error)
+	ReadPB(sectionId int64) (ProductBatchesResponse, error)
 }
 
-/* 
+/*
  * -- DATABASE QUERIES
  */
 const (
@@ -153,6 +111,6 @@ const (
 	sqlUpdateSection = `
 		UPDATE mercadofresco.sections 
 		SET section_number=?,current_capacity=?,current_temperature=?,maximum_capacity=?,minimum_capacity=?,
-		minimum_temperature=?,product_type_id=?, warehouse_id=? WHERE id = ?;`
-	sqlDeleteSection = `DELETE FROM mercadofresco.sections WHERE id = ?;`
+		minimum_temperature=?,product_type_id=?, warehouse_id=? WHERE id=?`
+	sqlDeleteSection = `DELETE FROM mercadofresco.sections WHERE id=?`
 )
--- internal/section/repository.go
+++ internal/section/repository.go
@@ -2,6 +2,9 @@ package section
 
 import (
 	"database/sql"
+	"errors"
+
+	"github.com/cpereira42/mercado-fresco-pron4/internal/warehouse"
 )
 
 type repository struct {
@@ -11,7 +14,6 @@ type repository struct {
 func NewRepository(db *sql.DB) Repository {
 	return &repository{db: db}
 }
- 
 
 func (repoSection *repository) ListarSectionAll() ([]Section, error) {
 	var sectionList []Section = []Section{}
@@ -42,7 +44,7 @@ func (repoSection *repository) ListarSectionAll() ([]Section, error) {
 	return sectionList, nil
 }
 
-func (repoSection *repository) ListarSectionOne(id int) (Section, error) {
+func (repoSection *repository) ListarSectionOne(id int64) (Section, error) {
 	rows := repoSection.db.QueryRow(sqlSelectByID, id)
 
 	sectionObj := Section{}
@@ -56,6 +58,14 @@ func (repoSection *repository) ListarSectionOne(id int) (Section, error) {
 }
 
 func (repoSection *repository) CreateSection(newSection Section) (Section, error) {
+	if _, err := repoSection.getWarhouseById(repoSection.db, newSection.WarehouseId); err != nil {
+		return Section{}, err
+	}
+
+	if _, err := repoSection.getProductBatchtes(repoSection.db, newSection.ProductTypeId); err != nil {
+		return Section{}, err
+	}
+
 	stmt, err := repoSection.db.Prepare(sqlCreateSection)
 	if err != nil {
 		return Section{}, err
@@ -75,19 +85,20 @@ func (repoSection *repository) CreateSection(newSection Section) (Section, error
 		return Section{}, err
 	}
 
-	newSection.Id = int(lastID)
+	newSection.Id = lastID
 
 	return newSection, nil
 }
 
-func (repoSection *repository) UpdateSection(id int, sectionUp Section) (Section, error) {
-	stmt, err := repoSection.db.Prepare(sqlUpdateSection)
-	if err != nil {
+func (repoSection *repository) UpdateSection(sectionUp Section) (Section, error) {
+	if _, err := repoSection.getWarhouseById(repoSection.db, sectionUp.WarehouseId); err != nil {
 		return Section{}, err
 	}
-	defer stmt.Close()
 
-	_, err = stmt.Exec(
+	if _, err := repoSection.getProductBatchtes(repoSection.db, sectionUp.ProductTypeId); err != nil {
+		return Section{}, err
+	}
+	_, err := repoSection.db.Exec(sqlUpdateSection,
 		&sectionUp.SectionNumber,
 		&sectionUp.CurrentCapacity,
 		&sectionUp.CurrentTemperature,
@@ -96,7 +107,7 @@ func (repoSection *repository) UpdateSection(id int, sectionUp Section) (Section
 		&sectionUp.MinimumTemperature,
 		&sectionUp.ProductTypeId,
 		&sectionUp.WarehouseId,
-		&id,
+		&sectionUp.Id,
 	)
 	if err != nil {
 		return Section{}, err
@@ -104,15 +115,39 @@ func (repoSection *repository) UpdateSection(id int, sectionUp Section) (Section
 	return sectionUp, nil
 }
 
-func (repoSection *repository) DeleteSection(id int) error {
+func (repoSection *repository) DeleteSection(id int64) error {
 	result, err := repoSection.db.Query(sqlDeleteSection, id)
 	if err != nil {
-		return err
-	} 
+		return errors.New("falha ao remove o sections")
+	}
 	defer result.Close()
 
 	if result.Err() != nil {
 		return result.Err()
 	}
 	return nil
+}
+
+func (repo *repository) getProductBatchtes(db *sql.DB, id int64) (ProductTypes, error) {
+	result := db.QueryRow("select id from products_types where id=?", id)
+	productTypes := ProductTypes{}
+	err := result.Scan(
+		&productTypes.ID,
+	)
+	if err != nil {
+		return productTypes, errors.New("product_type_id id not found")
+	}
+	return productTypes, nil
+}
+
+func (repo *repository) getWarhouseById(db *sql.DB, id int64) (warehouse.Warehouse, error) {
+	result := db.QueryRow("select id from warehouse where id=?", id)
+	warehouse := warehouse.Warehouse{}
+	err := result.Scan(
+		&warehouse.ID,
+	)
+	if err != nil {
+		return warehouse, errors.New("warehouse_id id not found")
+	}
+	return warehouse, nil
 }
