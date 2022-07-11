555097e8bc4dabb79897b808cef3ce5986951882
Vinicius Oliveira <runnice@gmail.com>
2022-07-11 15:51:34 -0300

    create TestServiceCreate and TestServiceGetAllReports

---
+++ internal/carries/service_test.go
@@ -0,0 +1,87 @@
+package carries_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/cpereira42/mercado-fresco-pron4/internal/carries"
+	"github.com/cpereira42/mercado-fresco-pron4/internal/carries/mocks"
+	"github.com/stretchr/testify/assert"
+	tmock "github.com/stretchr/testify/mock"
+)
+
+var carryOne = carries.Carries{"cid1", "companyname1", "rua 1", "11112222", 1}
+
+// var carry2 = carries.Carries{"cid2", "companyname2", "rua 2", "11112222", 1}
+
+var localityOne = carries.Localities{1, "São Paulo", 1}
+var localityTwo = carries.Localities{2, "Nova York", 2}
+
+var carriesGetReports = []carries.Localities{locality1, locality2}
+
+func TestServiceCreate(t *testing.T) {
+
+	t.Run("If all fields are valid, should return a new carry", func(t *testing.T) {
+		mockRepository := new(mocks.Repository)
+		mockRepository.On("Create",
+			tmock.AnythingOfType("string"),
+			tmock.AnythingOfType("string"),
+			tmock.AnythingOfType("string"),
+			tmock.AnythingOfType("string"),
+			tmock.AnythingOfType("int")).Return(carryOne, nil)
+
+		service := carries.NewService(mockRepository)
+
+		newCarry, err := service.Create("cid1", "companyname1", "rua 1", "11112222", 1)
+
+		assert.Nil(t, err)
+		assert.ObjectsAreEqualValues(carryOne, newCarry)
+	})
+
+	t.Run("If Create fail, should return a an error", func(t *testing.T) {
+		mockRepository := new(mocks.Repository)
+		errorMsg := "fail to create"
+
+		mockRepository.On("Create",
+			tmock.AnythingOfType("string"),
+			tmock.AnythingOfType("string"),
+			tmock.AnythingOfType("string"),
+			tmock.AnythingOfType("string"),
+			tmock.AnythingOfType("int")).Return(carries.Carries{}, errors.New(errorMsg))
+
+		service := carries.NewService(mockRepository)
+
+		_, err := service.Create("cid1", "companyname1", "rua 1", "11112222", 1)
+
+		assert.Error(t, err)
+
+	})
+}
+
+func TestServiceGetAllReport(t *testing.T) {
+	t.Run("Genearate a report of all localities - OK", func(t *testing.T) {
+		mockRepository := new(mocks.Repository)
+		mockRepository.On("GetAllReport").Return(carriesGetReports, nil)
+
+		service := carries.NewService(mockRepository)
+
+		carries, err := service.GetAllReport()
+
+		assert.Nil(t, err)
+		assert.ObjectsAreEqualValues(carriesReports, carries)
+	})
+	t.Run("Try to Genereate a report of all localities - Fail", func(t *testing.T) {
+		mockRepository := new(mocks.Repository)
+		errorMsg := "fail to get all localities"
+
+		mockRepository.On("GetAllReport").Return([]carries.Localities{}, errors.New(errorMsg))
+
+		service := carries.NewService(mockRepository)
+
+		_, err := service.GetAllReport()
+
+		assert.Error(t, err)
+
+	})
+
+}
