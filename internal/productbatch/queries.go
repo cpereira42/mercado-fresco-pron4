49b175b4c6aacb8d8a36c68bb5e725a9aa7ee5c5
eneassena10 <eneas.sena@mercadolivre.com>
2022-07-14 16:24:19 -0300

    alter querys sql product_batches

--- internal/productbatch/queries.go
+++ internal/productbatch/queries.go
@@ -3,6 +3,6 @@ package productbatch
 const (
 	SqlCreatePB          = `INSERT INTO mercadofresco.products_batches (batch_number,current_quantity,current_temperature,due_date,initial_quantity,manufacturing_date,manufacturing_hour,minimum_temperature,product_id,section_id) VALUES (?,?,?,?,?,?,?,?,?,?)`
 	SqlBatcheNumber      = `SELECT batch_number FROM mercadofresco.products_batches WHERE batch_number=?`
-	SqlrelatorioTodo     = `SELECT s.id AS 'section_id', s.section_number AS 'section_number', count(*) AS 'products_count' FROM mercadofresco.products_batches as pbs INNER JOIN mercadofresco.sections AS s ON s.id = pbs.section_id INNER JOIN mercadofresco.products AS pcts ON pbs.product_id = pcts.id GROUP BY s.id`
-	SqlrelatorioSectioId = `SELECT s.id AS 'section_id', s.section_number, count(*) as 'products_count' from mercadofresco.products_batches as pbs INNER JOIN mercadofresco.sections as s on s.id = pbs.section_id INNER JOIN mercadofresco.products as pcts on pbs.product_id = pcts.id WHERE s.id=?`
+	SqlrelatorioTodo     = `SELECT s.id AS 'section_id', s.section_number AS 'section_number', count(s.id) AS 'products_count' FROM bgow1s413.products_batches as pbs INNER JOIN bgow1s413.sections AS s ON s.id = pbs.section_id INNER JOIN bgow1s413.products AS pcts ON pbs.product_id = pcts.id group by s.id having count(s.id) >= 0;`
+	SqlrelatorioSectioId = `SELECT s.id AS 'section_id', s.section_number AS 'section_number', count(s.id) AS 'products_count' FROM bgow1s413.products_batches as pbs INNER JOIN bgow1s413.sections AS s ON s.id = pbs.section_id INNER JOIN bgow1s413.products AS pcts ON pbs.product_id = pcts.id where s.id=? group by s.id having count(s.id) >= 0`
 )
